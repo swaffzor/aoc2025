ef192b4d6cbe6cbb3e56fcd52578a8c6ff6bab79
Jeremy Swafford <jeremy@swafford.co>
2025-12-02 11:42:05 -0500

    soooooo close, i can feel it

--- day1/solution.ts
+++ day1/solution.ts
@@ -47,6 +47,37 @@ Because the dial points at 0 a total of three times during this process, the pas
 Analyze the rotations in your attached document. What's the actual password to open the door?
 */
 
+/*
+--- Part Two ---
+
+You're sure that's the right password, but the door won't open. You knock, but nobody answers. You build a snowman while you think.
+
+As you're rolling the snowballs for your snowman, you find another security document that must have fallen into the snow:
+
+"Due to newer security protocols, please use password method 0x434C49434B until further notice."
+
+You remember from the training seminar that "method 0x434C49434B" means you're actually supposed to count the number of times any click causes the dial to point at 0, regardless of whether it happens during a rotation or at the end of one.
+
+Following the same rotations as in the above example, the dial points at zero a few extra times during its rotations:
+
+The dial starts by pointing at 50.
+The dial is rotated L68 to point at 82; during this rotation, it points at 0 once.
+The dial is rotated L30 to point at 52.
+The dial is rotated R48 to point at 0.
+The dial is rotated L5 to point at 95.
+The dial is rotated R60 to point at 55; during this rotation, it points at 0 once.
+The dial is rotated L55 to point at 0.
+The dial is rotated L1 to point at 99.
+The dial is rotated L99 to point at 0.
+The dial is rotated R14 to point at 14.
+The dial is rotated L82 to point at 32; during this rotation, it points at 0 once.
+In this example, the dial points at 0 three times at the end of a rotation, plus three more times during a rotation. So, in this example, the new password would be 6.
+
+Be careful: if the dial were pointing at 50, a single rotation like R1000 would cause the dial to point at 0 ten times before returning back to 50!
+
+Using password method 0x434C49434B, what is the password to open the door?
+*/
+
 export const day1part1 = (raw: string) => {
   const input = raw.split("\n");
 
@@ -78,3 +109,59 @@ const turnDial = (dialPosition: number, direction: string, offset: number) => {
   if (target > 99 || target <= 0) console.log({ operation, distance, target });
   return target >= 100 ? target - 100 : target < 0 ? target + 100 : target;
 };
+
+export const day1part2 = (raw: string) => {
+  const input = raw.split("\n");
+
+  const temp = input.reduce(
+    (dial, curr) => {
+      const newPos = turnDial2(
+        dial.value,
+        curr.charAt(0),
+        Number(curr.substring(1))
+      );
+      return { count: dial.count + newPos.count, value: newPos.value };
+    },
+    { value: 50, count: 0 }
+  );
+
+  return temp.count;
+};
+
+const turnDial2 = (dialPosition: number, direction: string, offset: number) => {
+  // offset < 0, no op
+  // offset < 100, count += dial.offset + offset
+  // offset > 100, count += offset/100
+  const operation = ["L", "l"].includes(direction) ? -1 : 1;
+  // const stringOffset = offset.toString();
+  // const substringed = stringOffset.substring(stringOffset.length - 2);
+  const dividedCount = Math.floor(offset / 100);
+  const remainingOffset = offset % 100;
+  const count = offset > 99 ? dividedCount : 0;
+  const distance = operation * (dividedCount > 0 ? remainingOffset : offset);
+  const target = dialPosition + distance;
+  const bonusPoint = target === 0 || target === 100 ? 1 : 0;
+
+  const endPos =
+    target > 99
+      ? { count: count + 1, value: target - 100 }
+      : target < 0
+      ? { count: count + 1, value: target + 100 }
+      : { count: count + bonusPoint, value: target };
+
+  console.log({
+    dialPosition,
+    direction,
+    offset,
+    dividedCount,
+    distance,
+    target,
+    endPos,
+  });
+
+  return endPos;
+};
+// 4334 is too low
+// 6591 is incorrect
+// 7163 is incorrect
+// 26282 is too high
--- day1/test.ts
+++ day1/test.ts
@@ -1,13 +1,42 @@
 import { readInputForDay } from "../utils";
-import { day1part1 } from "./solution";
+import { day1part1, day1part2 } from "./solution";
 
 describe("day1", () => {
-  it("part 1 solution", () => {
+  it.skip("part 1 solution", () => {
     expect(day1part1(readInputForDay(1, true))).toEqual(3);
     console.log("day1part1:", day1part1(readInputForDay(1)));
   });
-  // it("part2 solution", () => {
-  //   expect(day1part2(readInput(5, true))).toEqual(123);
-  //   console.log("day1part2:", day1part2(readInput(5)));
-  // });
+
+  it("part2 left over 100 clicks", () => {
+    const input = `L168
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82`;
+    expect(day1part2(input)).toEqual(7);
+  });
+
+  it.only("part2 right over 100 clicks", () => {
+    const input = `L68
+L30
+R148
+L5
+R60
+L55
+L1
+L99
+R14
+L82`;
+    expect(day1part2(input)).toEqual(7);
+  });
+
+  it("part2 solution", () => {
+    // expect(day1part2(readInputForDay(1, true))).toEqual(6);
+    console.log("ANSWER:", day1part2(readInputForDay(1)));
+  });
 });
