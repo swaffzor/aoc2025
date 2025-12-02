ed2bfb7f170995d61e0da7d3567c7ca7c1422392
Jeremy Swafford <jeremy@swafford.co>
2025-12-02 11:47:48 -0500

    setup day2

---
+++ day2/test.ts
@@ -0,0 +1,9 @@
+import { readInputForDay } from "../utils";
+import { day2part1 } from "./solution";
+
+  it("part 1 solution", () => {
+    expect(day2part1(readInputForDay(2, true))).toEqual(1227775554);
+    console.log("day2part1 ANSWER:", day2part1(readInputForDay(2)));
+describe("day2", () => {
+  });
+});
