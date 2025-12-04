b4de6fbc958c7a9036590630d9e216e13025f6b9
Jeremy Swafford <jeremy@swafford.co>
2025-12-04 09:48:30 -0500

    day4 part2 complete

--- day4/solution.ts
+++ day4/solution.ts
@@ -39,8 +39,188 @@ x.@@@.@@@@
 x.x.@@@.x.
 Consider your complete diagram of the paper roll locations. How many rolls of paper can be accessed by a forklift?
  */
+/*
+--- Part Two ---
 
+Now, the Elves just need help accessing as much of the paper as they can.
+
+Once a roll of paper can be accessed by a forklift, it can be removed. Once a roll of paper is removed, the forklifts might be able to access more rolls of paper, which they might also be able to remove. How many total rolls of paper could the Elves remove if they keep repeating this process?
+
+Starting with the same example as above, here is one way you could remove as many rolls of paper as possible, using highlighted @ to indicate that a roll of paper is about to be removed, and using x to indicate that a roll of paper was just removed:
+
+Initial state:
+..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.
+
+Remove 13 rolls of paper:
+..xx.xx@x.
+x@@.@.@.@@
+@@@@@.x.@@
+@.@@@@..@.
+x@.@@@@.@x
+.@@@@@@@.@
+.@.@.@.@@@
+x.@@@.@@@@
+.@@@@@@@@.
+x.x.@@@.x.
+
+Remove 12 rolls of paper:
+.......x..
+.@@.x.x.@x
+x@@@@...@@
+x.@@@@..x.
+.@.@@@@.x.
+.x@@@@@@.x
+.x.@.@.@@@
+..@@@.@@@@
+.x@@@@@@@.
+....@@@...
+
+Remove 7 rolls of paper:
+..........
+.x@.....x.
+.@@@@...xx
+..@@@@....
+.x.@@@@...
+..@@@@@@..
+...@.@.@@x
+..@@@.@@@@
+..x@@@@@@.
+....@@@...
+
+Remove 5 rolls of paper:
+..........
+..x.......
+.x@@@.....
+..@@@@....
+...@@@@...
+..x@@@@@..
+...@.@.@@.
+..x@@.@@@x
+...@@@@@@.
+....@@@...
+
+Remove 2 rolls of paper:
+..........
+..........
+..x@@.....
+..@@@@....
+...@@@@...
+...@@@@@..
+...@.@.@@.
+...@@.@@@.
+...@@@@@x.
+....@@@...
+
+Remove 1 roll of paper:
+..........
+..........
+...@@.....
+..x@@@....
+...@@@@...
+...@@@@@..
+...@.@.@@.
+...@@.@@@.
+...@@@@@..
+....@@@...
+
+Remove 1 roll of paper:
+..........
+..........
+...x@.....
+...@@@....
+...@@@@...
+...@@@@@..
+...@.@.@@.
+...@@.@@@.
+...@@@@@..
+....@@@...
+
+Remove 1 roll of paper:
+..........
+..........
+....x.....
+...@@@....
+...@@@@...
+...@@@@@..
+...@.@.@@.
+...@@.@@@.
+...@@@@@..
+....@@@...
+
+Remove 1 roll of paper:
+..........
+..........
+..........
+...x@@....
+...@@@@...
+...@@@@@..
+...@.@.@@.
+...@@.@@@.
+...@@@@@..
+....@@@...
+Stop once no more rolls of paper are accessible by a forklift. In this example, a total of 43 rolls of paper can be removed.
+
+Start with your original diagram. How many rolls of paper in total can be removed by the Elves and their forklifts?
+*/
+
+import { Point } from "types";
 import { extractDataToPointGrid, getPointNeighbors } from "../utils";
+
+export const day4part2 = (raw: string) => {
+  const grid = extractDataToPointGrid<string>(raw);
+
+  let loopGrid: Point<string>[][] = [...grid];
+
+  let sum = 0;
+  let loop = true;
+  while (loop) {
+    const result = removeRolls(loopGrid);
+    const stringGrid = resultString(result.newGrid);
+    console.log(result.count);
+    console.log(stringGrid);
+    sum = sum + result.count;
+    loop = result.count > 0;
+    loopGrid = result.newGrid;
+  }
+
+  return sum;
+};
+
+export const removeRolls = (grid: Point<string>[][]) => {
+  let count = 0;
+  const newGrid = grid.map((r) =>
+    r.map((p) => {
+      if (p.value !== "@") return p;
+      const neighbors = getPointNeighbors(p, grid, true);
+      const toRemove = neighbors.filter((p) => p.value === "@").length < 4;
+      if (toRemove) {
+        count = count + 1;
+        return { ...p, value: "." };
+      } else {
+        return p;
+      }
+    }, 0)
+  );
+
+  return { count, newGrid };
+};
+
+export const resultString = (g: Point<string>[][]) =>
+  g
+    .map((r) => {
+      const newRow = r.map((p) => p.value);
+      return newRow.join("").concat("\n");
+    })
+    .join("");
 
 export const day4part1 = (raw: string) => {
   const grid = extractDataToPointGrid<string>(raw);
--- day4/test.ts
+++ day4/test.ts
@@ -1,16 +1,158 @@
-import { readInputForDay } from "../utils";
-import { day4part1 } from "./solution";
+import { Point } from "types";
+import { extractDataToPointGrid, readInputForDay } from "../utils";
+import { day4part1, day4part2, removeRolls, resultString } from "./solution";
 
+const grids = [
+  // Initial state:
+  `..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.
+`,
+  // Remove 13 rolls of paper:
+  `.......@..
+.@@.@.@.@@
+@@@@@...@@
+@.@@@@..@.
+.@.@@@@.@.
+.@@@@@@@.@
+.@.@.@.@@@
+..@@@.@@@@
+.@@@@@@@@.
+....@@@...
+`,
+  // Remove 12 rolls of paper:
+  `..........
+.@@.....@.
+.@@@@...@@
+..@@@@....
+.@.@@@@...
+..@@@@@@..
+...@.@.@@@
+..@@@.@@@@
+..@@@@@@@.
+....@@@...
+`,
+  // Remove 7 rolls of paper:
+  `..........
+..@.......
+.@@@@.....
+..@@@@....
+...@@@@...
+..@@@@@@..
+...@.@.@@.
+..@@@.@@@@
+...@@@@@@.
+....@@@...
+`,
+  // Remove 5 rolls of paper:
+  `..........
+..........
+..@@@.....
+..@@@@....
+...@@@@...
+...@@@@@..
+...@.@.@@.
+...@@.@@@.
+...@@@@@@.
+....@@@...
+`,
+  // Remove 2 rolls of paper:
+  `..........
+..........
+...@@.....
+..@@@@....
+...@@@@...
+...@@@@@..
+...@.@.@@.
+...@@.@@@.
+...@@@@@..
+....@@@...
+`,
+  // Remove 1 roll of paper:
+  `..........
+..........
+...@@.....
+...@@@....
+...@@@@...
+...@@@@@..
+...@.@.@@.
+...@@.@@@.
+...@@@@@..
+....@@@...
+`,
+  // Remove 1 roll of paper:
+  `..........
+..........
+....@.....
+...@@@....
+...@@@@...
+...@@@@@..
+...@.@.@@.
+...@@.@@@.
+...@@@@@..
+....@@@...
+`,
+  // Remove 1 roll of paper:
+  `..........
+..........
+..........
+...@@@....
+...@@@@...
+...@@@@@..
+...@.@.@@.
+...@@.@@@.
+...@@@@@..
+....@@@...
+`,
+  // Remove 1 roll of paper:
+  `..........
+..........
+..........
+....@@....
+...@@@@...
+...@@@@@..
+...@.@.@@.
+...@@.@@@.
+...@@@@@..
+....@@@...
+`,
+];
 describe("day4", () => {
-  describe("part1", () => {
+  describe.skip("part1", () => {
     it("solution", () => {
       expect(day4part1(readInputForDay(4, true))).toEqual(13);
       console.log("day4part1 ANSWER:", day4part1(readInputForDay(4)));
     });
   });
 
-  // it("part2 solution", () => {
-  //   expect(day4part2(readInputForDay(4, true))).toEqual(6);
-  //   console.log("day4part2 ANSWER:", day4part2(readInputForDay(4)));
-  // });
+  it.skip.each([
+    [0, grids[0], grids[1]],
+    [1, grids[1], grids[2]],
+    [2, grids[2], grids[3]],
+    [3, grids[3], grids[4]],
+    [4, grids[4], grids[5]],
+    [5, grids[5], grids[6]],
+    [6, grids[6], grids[7]],
+    [7, grids[7], grids[8]],
+    [8, grids[8], grids[9]],
+  ])("removeRolls %i", (_index, input, expected) => {
+    const pointGrid = extractDataToPointGrid<string>(input);
+    const result = removeRolls(pointGrid);
+
+    const temp = resultString(result.newGrid);
+    console.log(temp);
+    expect(temp).toEqual(expected);
+  });
+
+  it("part2 solution", () => {
+    expect(day4part2(readInputForDay(4, true))).toEqual(43);
+    console.log("day4part2 ANSWER:", day4part2(readInputForDay(4)));
+  });
 });
