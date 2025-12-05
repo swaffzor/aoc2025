ddd17be7ff7b2b60ad8a33dc02f61a6fbcd6bbdd
Jeremy Swafford <jeremy@swafford.co>
2025-12-05 11:28:03 -0500

    day5 part2 setup

--- day5/test.ts
+++ day5/test.ts
@@ -1,16 +1,16 @@
-import { day5part1 } from "./solution";
 import { readInputForDay } from "../utils";
+import { day5part1, day5part2 } from "./solution";
 
 describe("day5", () => {
-  describe("part1", () => {
+  describe.skip("part1", () => {
     it("solution", () => {
       expect(day5part1(readInputForDay(5, true))).toEqual(3);
       console.log("day5part1 ANSWER:", day5part1(readInputForDay(5)));
     });
   });
 
-  // it("part2 solution", () => {
-  //   expect(day5part2(readInputForDay(5, true))).toEqual(43);
-  //   console.log("day5part2 ANSWER:", day5part2(readInputForDay(5)));
-  // });
+  it("part2 solution", () => {
+    expect(day5part2(readInputForDay(5, true))).toEqual(14);
+    console.log("day5part2 ANSWER:", day5part2(readInputForDay(5)));
+  });
 });
